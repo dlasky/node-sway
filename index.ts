e47dd907ff11ba496e25daff0b58cdb3f868aa1a
Dan Lasky <github@aerolith.net>
2022-08-12 21:01:02 -0400

    better message handling

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import net from 'net'
 import { EventEmitter } from 'events'
-import { message, Types } from './message'
+import { writeMessage, readMessage, Types } from './message'
 import type {MsgType} from "./message"
 
 export default class SwayIPC extends EventEmitter {
@@ -16,16 +16,18 @@ export default class SwayIPC extends EventEmitter {
     }
 
     handleData = (data : Buffer) => {
-        const str = data.slice(14, data.length).toString()
+        this.emit("rawData", data.toString())
+        const msg = readMessage(data)
         try {
-            this.emit("data", JSON.parse(str))
+            const parsed = JSON.parse(msg.value)
+            this.emit("data", parsed)
         } catch (e) {
             this.emit("error", e)
         }
     }
 
     send = (type:MsgType, value: string) => {
-        const msg = message(type, value)
+        const msg = writeMessage(type, value)
         this.socket.write(msg)
     }
 }
--- message.ts
+++ message.ts
@@ -1,9 +1,9 @@
-import os from 'os'
+import os from "os";
 
-export type MsgType = number
-export type MsgTypes = Record<string,MsgType>
+export type MsgType = number;
+export type MsgTypes = Record<string, MsgType>;
 
-export const Types : MsgTypes = {
+export const Types: MsgTypes = {
   RUN_COMMAND: 0 as MsgType, //Runs the payload as sway commands
   GET_WORKSPACES: 1, //Get the list of current workspaces
   SUBSCRIBE: 2, //Subscribe the IPC connection to the events listed in the payload
@@ -21,22 +21,52 @@ export const Types : MsgTypes = {
   GET_SEATS: 101, //Get the list of seats
 };
 
-const magic = Buffer.from('i3-ipc')
+const magic = Buffer.from("i3-ipc");
 
-const end = os.endianness()
+const end = os.endianness();
 
-const writeInt32 = (buf:Buffer, value:number, offset?:number) => {
+const writeInt32 = (buf: Buffer, value: number, offset?: number) => {
   if (end === "BE") {
-  return buf.writeInt32BE(value, offset)
+    return buf.writeInt32BE(value, offset);
   }
-  return buf.writeInt32LE(value, offset)
+  return buf.writeInt32LE(value, offset);
+};
+
+const readInt32 = (buf:Buffer, offset?:number) => {
+  if (end === "BE") {
+    return buf.readInt32BE(offset)
+  }
+  return buf.readInt32LE(offset)
 }
 
-export const message = (type : MsgType, value : string) => {
-    const msg = Buffer.from(value ?? '')
-    const len = msg.length
-    const buf = Buffer.alloc(8)
-    writeInt32(buf,len)
-    writeInt32(buf, type, 4)
-    return Buffer.concat([magic, buf, msg])
+//<magic><i32 len><i32 type><msg>
+
+export const writeMessage = (type: MsgType, value: string) => {
+  const msg = Buffer.from(value ?? "");
+  const len = msg.length;
+  const buf = Buffer.alloc(8);
+  writeInt32(buf, len);
+  writeInt32(buf, type, 4);
+  return Buffer.concat([magic, buf, msg]);
+};
+
+type RawMessage = {
+  type: MsgType
+  value: string
 }
+
+export const readMessage = (buf: Buffer) : RawMessage => {
+  if (magic.compare(buf, 0, magic.length)===0) {
+      let lenOffset = magic.length
+      const len = buf.readInt32LE(lenOffset)
+      const typ = buf.readInt32LE(lenOffset+4)
+      const msg = buf.subarray(lenOffset+8, len+6+8)
+
+      return {
+        type:typ,
+        value:msg.toString()
+      }
+  } else {
+    throw(new Error("message is missing magic string:"+ buf.toString()))
+  }
+};
